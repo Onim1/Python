12d74134ff09f2cf93eb23bf740295327b238338
Dewan M <89687855+Onim1@users.noreply.github.com>
2024-12-11 21:41:32 +0900

    add new file

---
+++ Algorithm/python.py
@@ -0,0 +1,10 @@
+def strcomparagom(str):
+    startIndex = 0
+    endIndex = len(str) - 1
+
+    for x in str:
+        if str[startIndex] != str[endIndex]:
+            return True
+    return False
+
+print(strcomparagom("racecars"))
